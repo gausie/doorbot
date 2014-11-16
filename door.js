0e49831da05a80abdb77192a7d7ddf51d6f56ad2
Sam Gaus <git@gaus.ie>
2014-11-16 20:08:54 +0000

    Removed promisifcation from pcsc, it can't handle it

--- door.js
+++ door.js
@@ -10,69 +10,62 @@ module.exports = function () {
   var pcsc = pcsclite();
 
   pcsc.on('reader', function (reader) {
-
-    Promise.promisifyAll(reader);
-
     reader.on('status', function (status) {
       var changes = this.state ^ status.state;
       if (changes) {
-          if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
-            reader.disconnect();
-          } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+        if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+          reader.disconnect();
+        } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
 
-              reader.connect().then(function (protocol) {
+          reader.connect(function (protocol) {
 
-                // Create a protocol-aware function to transmit APDUs to
-                // the reader. We can pass this function to our action
-                // modules.
-                var reader_transmit = function (instruction) {
-                  // Convert hex string into byte array
-                  var byte_array = instruction.match(/(..?)/g).map(function(pair) {
-                    return parseInt(pair, 16);
-                  });
-                  return reader.transmit(new Buffer(byte_array), 40, protocol);
-                };
+            // Create a protocol-aware function to transmit APDUs to
+            // the reader. We can pass this function to our action
+            // modules.
+            var reader_transmit = function (instruction) {
+              // Convert hex string into byte array
+              var byte_array = instruction.match(/(..?)/g).map(function(pair) {
+                return parseInt(pair, 16);
+              });
+              return reader.transmit(new Buffer(byte_array), 40, protocol);
+            };
 
-                // APDU for GetCardUid
-                return reader_transmit('FFCA000000').then(function (data) {
+            // APDU for GetCardUid
+            return reader_transmit('FFCA000000').then(function (data) {
 
-                  var status = data.slice(-2);
-                  if(status.toString('hex',0,1) != 90){
-                    throw new Error("Reader returned error:" + status);
-                  }else{
+              var status = data.slice(-2);
+              if(status.toString('hex',0,1) != 90){
+                throw new Error("Reader returned error:" + status);
+              }else{
 
-                    var uid = data.slice(0,-2).toString('hex').toLowerCase();
-                    models.Card.findOne({
-                      where: {
-                        uid: uid
-                      },
-                      attributes: ['name', 'resident'],
-                      include: [{
-                        model: models.User,
-                        attributes: [],
-                        where: {
-                          enabled: true
-                        }
-                      }]
-                    }).then(function(entrant) {
+                var uid = data.slice(0,-2).toString('hex').toLowerCase();
+                models.Card.findOne({
+                  where: {
+                    uid: uid
+                  },
+                  attributes: ['name', 'resident'],
+                  include: [{
+                    model: models.User,
+                    attributes: [],
+                    where: {
+                      enabled: true
+                    }
+                  }]
+                }).then(function(entrant) {
 
-                      actions.run(reader_transmit, entrant, uid);
-
-                    });
-
-                  }
+                  actions.run(reader_transmit, entrant, uid);
 
                 });
 
+              }
 
-              }).catch(function(error) {
-                console.error(error);
-              });
+            });
 
-          }
+          });
+
+        }
       }
     });
-
   });
 
   pcsc.on('error', function(error) {
